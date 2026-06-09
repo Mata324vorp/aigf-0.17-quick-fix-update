4c88910a79a5ea805adf2f73ae8d1256eb6fbf34
Mata324vorp <oqasee@gmail.com>
2026-06-09 17:21:36 -0500

    Delete src/main directory

--- src/main/java/com/litewer/aigf/client/ai/CompanionAiResult.java
+++
@@ -1,7 +0,0 @@
-package com.litewer.aigf.client.ai;
-
-import com.litewer.aigf.entity.CompanionActionIntent;
-import com.litewer.aigf.entity.CompanionEmotion;
-
-public record CompanionAiResult(String spokenText, CompanionEmotion emotion, CompanionActionIntent actionIntent, String memoryFact, String careHint) {
-}
